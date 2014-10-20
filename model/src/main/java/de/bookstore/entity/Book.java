abb8150854edc57f5830b1b883d76e25991c2fb2
Michael Prankl <eidottermihi@gmail.com>
2014-10-20 20:25:47 +0200

    entities angelegt

---
+++ model/src/main/java/de/bookstore/entity/Book.java
@@ -0,0 +1,50 @@
+package de.bookstore.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column
+	private String title;
+	@Column
+	private int year;
+	@ManyToOne(optional = false, fetch = FetchType.EAGER)
+	private Author author;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	
+	
+}
