2e2a5ffdc2352f32e865a386975c6e300a392a3e
Michael Prankl <eidottermihi@gmail.com>
2015-01-10 10:48:31 +0100

    Acceptance tests.

--- web/src/main/java/de/bookstore/web/page/BasePage.java
+++ web/src/main/java/de/bookstore/web/page/BasePage.java
@@ -1,10 +1,27 @@
 package de.bookstore.web.page;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.springframework.core.io.ClassPathResource;
 
 public class BasePage extends WebPage {
 
   private static final long serialVersionUID = 1L;
 
-  
+  public BasePage() {
+    ClassPathResource classPathResource = new ClassPathResource("buildInfo.properties");
+    Properties p = new Properties();
+    String version = "";
+    try {
+      p.load(classPathResource.getInputStream());
+      version = p.getProperty("version");
+    } catch (IOException e) {
+      // buildInfo not present
+    }
+    add(new Label("version", version));
+  }
+
 }
